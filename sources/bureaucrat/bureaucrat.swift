671c7df86cd9ff18a1bd80552a79c56b3fcf17db
kelvin13 <kelvin13ma@gmail.com>
2022-02-24 23:34:53 -0600

    git integration

--- sources/bureaucrat/bureaucrat.swift
+++ sources/bureaucrat/bureaucrat.swift
@@ -1,11 +1,188 @@
-import SystemPackage
+@_exported @preconcurrency import struct SystemPackage.FilePath
+import SystemPackage 
+import Resource
 
 public 
-enum Bureaucrat 
+struct Bureaucrat:Sendable 
 {
-    public static 
-    func version(of file:FilePath, using git:FilePath = "/usr/bin/git")
+    public 
+    struct FileError:Error, CustomStringConvertible 
     {
+        let path:FilePath
+        let error:Error 
         
+        @usableFromInline 
+        init(path:FilePath, error:Error )
+        {
+            self.path   = path 
+            self.error  = error
+        }
+        
+        public 
+        var description:String 
+        {
+            "\(self.error) (path: \(self.path))"
+        }
+    }
+    
+    private 
+    let git:FilePath 
+    
+    public
+    init(git:FilePath = "/usr/bin/git")
+    {
+        self.git = git
+    }
+
+    private 
+    func modified(_ path:FilePath) async throws -> Bool
+    {
+        var output:String = try await Self.run(self.git, ["status", "-s", path.string])
+        while let last:Character = output.last
+        {
+            guard last.isWhitespace 
+            else 
+            {
+                return true
+            }
+            output.removeLast()
+        }
+        return false 
+    }
+    private 
+    func lastCommit(_ path:FilePath) async throws -> String
+    {
+        var output:String = try await Self.run(self.git, ["log", "-n", "1", "--format=%H", path.string])
+        while case true? = output.last?.isWhitespace 
+        {
+            output.removeLast()
+        }
+        return output
+    }
+    
+    public 
+    func version(of path:FilePath) async throws -> Resource.Version?
+    {
+        async let commit:String = self.lastCommit(path)
+        if try await self.modified(path)
+        {
+            return nil
+        }
+        return .init(try await commit)
+    }
+    
+    public
+    func read(from path:FilePath, type:Resource.Text) async throws -> Resource
+    {
+        async let version:Resource.Version? = self.version(of: path)
+        let bytes:[UInt8]                   = try Self.read([UInt8].self, from: path)
+        return .bytes(bytes, type: type, version: try await version)
+    }
+    public
+    func read(from path:FilePath, type:Resource.Binary) async throws -> Resource
+    {
+        async let version:Resource.Version? = self.version(of: path)
+        let bytes:[UInt8]                   = try Self.read([UInt8].self, from: path)
+        return .binary(bytes, type: type, version: try await version)
+    }
+    
+    @inlinable public static 
+    func read(_:[UInt8].Type = [UInt8].self, from path:FilePath) throws -> [UInt8]
+    {
+        try Self.read(from: path)
+        {
+            (file:FileDescriptor, count:Int) in 
+            try .init(unsafeUninitializedCapacity: count)
+            {
+                $1 = try file.read(fromAbsoluteOffset: 0, into: UnsafeMutableRawBufferPointer.init($0))
+            }
+        }
+    }
+    @inlinable public static 
+    func read(_:String.Type = String.self, from path:FilePath) throws -> String
+    {
+        try Self.read(from: path)
+        {
+            (file:FileDescriptor, count:Int) in 
+            try .init(unsafeUninitializedCapacity: count)
+            {
+                try file.read(fromAbsoluteOffset: 0, into: UnsafeMutableRawBufferPointer.init($0))
+            }
+        }
+    }
+    @inlinable public static 
+    func read<T>(from path:FilePath, _ initializer:(FileDescriptor, Int) throws -> T) throws -> T
+    {
+        do 
+        {
+            let file:FileDescriptor = try .open(path, .readOnly)
+            return try file.closeAfter 
+            {
+                return try initializer(file, Int.init(try file.seek(offset: 0, from: .end)))
+            }
+        }
+        catch let error 
+        {
+            throw FileError.init(path: path, error: error)
+        }
+    }
+    
+    public 
+    struct IncompleteWriteError:Error, CustomStringConvertible 
+    {
+        let wrote:Int, 
+            total:Int
+        
+        @usableFromInline 
+        init(wrote:Int, of total:Int)
+        {
+            self.wrote = wrote 
+            self.total = total
+        }
+        
+        public 
+        var description:String 
+        {
+            "could only write \(self.wrote) of \(self.total) byte(s)"
+        }
+    }
+    
+    @inlinable public static 
+    func write(_ buffer:UnsafeBufferPointer<UInt8>, to path:FilePath) throws 
+    {
+        do 
+        {
+            let file:FileDescriptor = try .open(path, .writeOnly, options: [.create, .truncate])
+            let count:Int           = try file.closeAfter 
+            {
+                try file.write(UnsafeRawBufferPointer.init(buffer))
+            }
+            guard count == buffer.count
+            else
+            {
+                throw IncompleteWriteError.init(wrote: count, of: buffer.count)
+            }
+        }
+        catch let error 
+        {
+            throw FileError.init(path: path, error: error)
+        }
+    }
+    @inlinable public static 
+    func write(_ array:[UInt8], to path:FilePath) throws
+    {
+        try array.withUnsafeBufferPointer { try Self.write($0, to: path) }
+    }
+    @inlinable public static 
+    func write(_ string:String, to path:FilePath) throws
+    {
+        var string:String = string 
+        try string.withUTF8 { try Self.write($0, to: path) }
+    }
+    // will make the string UTF-8 and contiguous 
+    @inlinable public static 
+    func write(_ string:inout String, to path:FilePath) throws
+    {
+        try string.withUTF8 { try Self.write($0, to: path) }
     }
 }
--- sources/bureaucrat/git.swift
+++ sources/bureaucrat/git.swift
@@ -1,12 +1,12 @@
-import SystemPackage
+import struct SystemPackage.FilePath
 
 import func SwiftShell.runAsync 
 import class SwiftShell.RunOutput
 import class SwiftShell.AsyncCommand
 
-enum Git 
+extension Bureaucrat
 {
-    private static 
+    static 
     func run(_ command:FilePath, _ arguments:[String]) async throws -> String 
     {
         try await withCheckedThrowingContinuation 
@@ -15,11 +15,11 @@ enum Git
             
             runAsync(command.string, arguments).onCompletion 
             {
-                (git:AsyncCommand) in 
+                (command:AsyncCommand) in 
                 do 
                 {
-                    try git.finish()
-                    continuation.resume(returning: git.stdout.read())
+                    try command.finish()
+                    continuation.resume(returning: command.stdout.read())
                 }
                 catch let error 
                 {
@@ -27,30 +27,5 @@ enum Git
                 }
             }
         }
-    }
-    static 
-    func modified(_ path:FilePath, using git:FilePath) async throws -> Bool
-    {
-        var output:String = try await Self.run(git, ["status", "-s", path.string])
-        while let last:Character = output.last
-        {
-            guard last.isWhitespace 
-            else 
-            {
-                return true
-            }
-            output.removeLast()
-        }
-        return false 
-    }
-    static 
-    func lastCommit(_ path:FilePath, using git:FilePath) async throws -> String
-    {
-        var output:String = try await Self.run(git, ["log", "-n", "1", "--format=%H", path.string])
-        while case true? = output.last?.isWhitespace 
-        {
-            output.removeLast()
-        }
-        return output
     }
 }
