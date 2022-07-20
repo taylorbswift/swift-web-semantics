c2cc315a1298a822fd77ada878f9fbd3b8ac4aa4
kelvin13 <kelvin13ma@gmail.com>
2022-07-19 19:16:51 -0500

    split SystemExtras into a different package, fixes #2

--- Package.swift
+++ Package.swift
@@ -1,42 +1,19 @@
 // swift-tools-version:5.5
 import PackageDescription
 
-#if os(Linux) || os(macOS) || os(Windows)
-let dependencies:[Package.Dependency] = 
-[
-    .package(url: "https://github.com/apple/swift-system.git",  from: "1.2.1"),
-]
-let products:[Product] = 
-[
-    .library(name: "SystemExtras",  targets: ["SystemExtras"]),
-]
-let targets:[Target] = 
-[
-    .target(name: "SystemExtras", 
-        dependencies: 
-        [
-            .product(name: "SystemPackage", package: "swift-system"),
-        ]),
-]
-#else 
-let dependencies:[Package.Dependency] = []
-let products:[Product] = []
-let targets:[Target] = []
-#endif 
-
 let package:Package = .init(
     name: "swift-resource",
-    products: products +
+    products: 
     [
         .library(name: "MIME",          targets: ["MIME"]),
         .library(name: "Resources",     targets: ["Resources"]),
         .library(name: "WebSemantics",  targets: ["WebSemantics"]),
     ],
-    dependencies: dependencies +
+    dependencies: 
     [
-        .package(url: "https://github.com/kelvin13/swift-hash.git", from: "0.2.2"),
+        .package(url: "https://github.com/kelvin13/swift-hash.git", from: "0.2.3"),
     ],
-    targets: targets +
+    targets: 
     [
         .target(name: "MIME"),
         .target(name: "Resources", 
